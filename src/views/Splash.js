c6713f7720b0b0994a5a8a782abe85cda5649c2b
Phil <philsometimes@gmail.com>
2020-12-28 12:14:47 -0500

    fix animation

--- src/views/Splash.js
+++ src/views/Splash.js
@@ -53,29 +53,27 @@ const Cursor = () => {
 
 const Name = ({horizontal}) => {
   return(
-    <div sx={{fontSize:'9vmin', fontFamily:'heading', color:'Teal1', textAlign:'right', marginX:'2vmin', gridArea:'name', justifySelf:'end', alignSelf:'end', lineHeight:'9vmin', width: horizontal ? '100%' : '50%', transition: 'text-wrap 2s', textWrap: horizontal ? 'no-wrap' : 'normal'}}>
+    <div sx={{fontSize:'9vmin', fontFamily:'heading', color:'Teal1', textAlign:'right', marginX:'2vmin', gridArea:'name', justifySelf:'end', alignSelf:'end', lineHeight:'9vmin', width: horizontal ? '100%' : '50%'}}>
       Lauren Davidson
     </div>
   )
 }
 
-const Developer = ({hx=0, hy=0, vx=0, vy=0, transition, horizontal}) => {
-  const animateDeveloper = keyframes`
-    0% {
-      translate(0,0)
+const Slider = ({type, hx=0, hy=0, vx=0, vy=0, horizontal}) => {
+  const animation = keyframes({
+    '0%': {
+      transform: 'translate(0px,0px)'
+    },
+    '50%': {
+      transform: `translate(${vx-hx}px,0px)`
+    },
+    '100%': {
+      transform: `translate(${vx-hx}px,${vy-hy}px)`
     }
-    50% {
-      translate(${vx}-${hx},0);
-      rotate(180deg)
-    }
-    100% {
-      translate(0,${vy}-${hy})
-    }
-  `
-  // const visible = x != 0 ? 'visible' : 'hidden'
+  })
   return (
-    <div sx={{position:'absolute', left:hx, top:hy, animation: !horizontal ? `${animateDeveloper} 1s infinite` : 'none'}}>
-      <TextBlock text='developer' color='Orange1'/>
+    <div sx={{position:'absolute', left:hx, top:hy, animation: !horizontal ? `${animation} 1s linear normal forwards` : 'none'}}>
+      <TextBlock text={type}/>
     </div>
   )
 }
@@ -84,7 +82,7 @@ const Developer = ({hx=0, hy=0, vx=0, vy=0, transition, horizontal}) => {
 
 const TextFlex = ({horizontal}) => {
   return (
-    <div sx={{height: horizontal ? 'auto' : '100%',width:'100%',display:'flex',flexDirection: horizontal ? 'row-reverse' : 'column', gridArea: horizontal ? 'horz' : 'vert', justifySelf: horizontal ? 'end' : 'start', alignSelf: horizontal ? 'start' : 'end', justifyContent:'space-between', paddingX: horizontal ? '2vmin' : 0}}>
+    <div sx={{visibility:'hidden', height: horizontal ? 'auto' : '100%',width:'100%',display:'flex',flexDirection: horizontal ? 'row-reverse' : 'column', gridArea: horizontal ? 'horz' : 'vert', justifySelf: horizontal ? 'end' : 'start', alignSelf: horizontal ? 'start' : 'end', justifyContent:'space-between', paddingX: horizontal ? '2vmin' : 0}}>
       <TextBlock id ={horizontal ? 'developerH' : 'developerV'}  text='developer'/>
       {horizontal &&
         <>
@@ -131,11 +129,9 @@ const TextBlock = ({text, id, color='Teal1'}) => {
 const Splash = () => {
   const [horizontal, setHorizontal] = useState(true)
   const [textPos, setTextPos] = useState(null)
-  const developerTransition = useRef(null)
   const triggerAnimation = () => {
-    setHorizontal(false)
+    setHorizontal(!horizontal)
     console.log(horizontal);
-    developerTransition.current = 'top 1s, left 1s'
   }
   const getPositions = useCallback( (el) => {
     const subtitles = document.getElementsByClassName('subtitle')
@@ -156,14 +152,16 @@ const Splash = () => {
       >
         <div sx={{height:'100%',width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
           <div sx={{height:'50px', width:'50px', backgroundColor:'red'}} onClick={()=>triggerAnimation()}></div>
-          <div ref={getPositions} sx={{height:'auto', width:'85vmin', display:'grid', gridTemplateAreas:"'name vert' 'horz .'"}}>
+          <div ref={getPositions} sx={{height:'auto', width:'85vmin', display:'grid', gridTemplateAreas:"'name vert' 'horz .'", gridTemplateRows:'18vmin auto', }}>
             <Name horizontal={horizontal}/>
-            <TextFlex horizontal={horizontal}/>
-            <TextFlex/>
+            <TextFlex horizontal={true}/>
+            <TextFlex horizontal={false}/>
           </div>
         </div>
         <Cursor />
-        <Developer hx={textPos?.developerH.x}  hy={textPos?.developerH.y} vx={textPos?.developerV.x} vy={textPos?.developerV.y} transition={developerTransition.current} horizontal={horizontal}/>
+        <Slider type='developer' hx={textPos?.developerH.x}  hy={textPos?.developerH.y} vx={textPos?.developerV.x} vy={textPos?.developerV.y} horizontal={horizontal}/>
+        <Slider type='educator' hx={textPos?.educatorH.x}  hy={textPos?.educatorH.y} vx={textPos?.educatorV.x} vy={textPos?.educatorV.y} horizontal={horizontal}/>
+        <Slider type='philosopher' hx={textPos?.philosopherH.x}  hy={textPos?.philosopherH.y} vx={textPos?.philosopherV.x} vy={textPos?.philosopherV.y} horizontal={horizontal}/>
       </div>
     </div>
 
