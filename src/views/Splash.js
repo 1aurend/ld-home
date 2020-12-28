49c15d558ffe63f998594cbed2f09899d3b1af4b
Phil <philsometimes@gmail.com>
2020-12-28 11:29:39 -0500

    tried to get keyframes working

--- src/views/Splash.js
+++ src/views/Splash.js
@@ -1,11 +1,11 @@
 /** @jsxImportSource theme-ui */
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect, useCallback, useRef} from 'react'
 import { jsx } from 'theme-ui'
+import { css, keyframes } from '@emotion/react'
 import testPng from '../assets/text1.png'
 
 const Cursor = () => {
   const [pos, setPos] = useState({x:0,y:0})
-  useEffect(()=>console.log(pos),[pos])
   useEffect(()=>{
     addEventListeners()
     return () => removeEventListeners()
@@ -51,31 +51,54 @@ const Cursor = () => {
   )
 }
 
-const Name = () => {
+const Name = ({horizontal}) => {
   return(
-    <div sx={{fontSize:'9vmin', fontFamily:'heading', color:'Teal1', textAlign:'right', marginX:'2vmin', gridArea:'name', justifySelf:'end', alignSelf:'end', lineHeight:'9vmin'}}>
+    <div sx={{fontSize:'9vmin', fontFamily:'heading', color:'Teal1', textAlign:'right', marginX:'2vmin', gridArea:'name', justifySelf:'end', alignSelf:'end', lineHeight:'9vmin', width: horizontal ? '100%' : '50%', transition: 'text-wrap 2s', textWrap: horizontal ? 'no-wrap' : 'normal'}}>
       Lauren Davidson
     </div>
   )
 }
 
+const Developer = ({hx=0, hy=0, vx=0, vy=0, transition, horizontal}) => {
+  const animateDeveloper = keyframes`
+    0% {
+      translate(0,0)
+    }
+    50% {
+      translate(${vx}-${hx},0);
+      rotate(180deg)
+    }
+    100% {
+      translate(0,${vy}-${hy})
+    }
+  `
+  // const visible = x != 0 ? 'visible' : 'hidden'
+  return (
+    <div sx={{position:'absolute', left:hx, top:hy, animation: !horizontal ? `${animateDeveloper} 1s infinite` : 'none'}}>
+      <TextBlock text='developer' color='Orange1'/>
+    </div>
+  )
+}
+
+
+
 const TextFlex = ({horizontal}) => {
   return (
-    <div sx={{height: horizontal ? 'auto' : '100%',width:'100%',display:'flex',flexDirection: horizontal ? 'row' : 'column', gridArea: horizontal ? 'horz' : 'vert', justifySelf: horizontal ? 'end' : 'start', alignSelf: horizontal ? 'start' : 'end', justifyContent:'space-between', paddingX: horizontal ? '2vmin' : 0}}>
+    <div sx={{height: horizontal ? 'auto' : '100%',width:'100%',display:'flex',flexDirection: horizontal ? 'row-reverse' : 'column', gridArea: horizontal ? 'horz' : 'vert', justifySelf: horizontal ? 'end' : 'start', alignSelf: horizontal ? 'start' : 'end', justifyContent:'space-between', paddingX: horizontal ? '2vmin' : 0}}>
       <TextBlock id ={horizontal ? 'developerH' : 'developerV'}  text='developer'/>
       {horizontal &&
         <>
-          <TextBlock text='<'/>
-          <Line></Line>
           <TextBlock text='>'/>
+          <Line></Line>
+          <TextBlock text='<'/>
         </>
       }
       <TextBlock id ={horizontal ? 'educatorH' : 'educatorV'} text='educator'/>
       {horizontal &&
         <>
-          <TextBlock text='<'/>
-          <Line></Line>
           <TextBlock text='>'/>
+          <Line></Line>
+          <TextBlock text='<'/>
         </>
       }
       <TextBlock id ={horizontal ? 'philosopherH' : 'philosopherV'} text='philosopher'/>
@@ -90,12 +113,12 @@ const Line = () => {
   )
 }
 
-const TextBlock = ({text, id}) => {
+const TextBlock = ({text, id, color='Teal1'}) => {
   return(
     <div
       id={id}
       className='subtitle'
-      sx={{height:'auto',width:'auto',fontFamily:'heading', fontSize:'2.5vmin',color:'Teal1'}}
+      sx={{height:'auto',width:'auto',fontFamily:'heading', fontSize:'2.5vmin',color:color}}
     >
       {text}
     </div>
@@ -103,16 +126,27 @@ const TextBlock = ({text, id}) => {
   )
 }
 
+
+
 const Splash = () => {
   const [horizontal, setHorizontal] = useState(true)
-  const getPositions = (el) => {
-    const subtitles = el.getElementsByClassName('subtitle')
+  const [textPos, setTextPos] = useState(null)
+  const developerTransition = useRef(null)
+  const triggerAnimation = () => {
+    setHorizontal(false)
+    console.log(horizontal);
+    developerTransition.current = 'top 1s, left 1s'
+  }
+  const getPositions = useCallback( (el) => {
+    const subtitles = document.getElementsByClassName('subtitle')
     const subArray = Array.from(subtitles).filter(item => item.id)
     const numbers = subArray.map(item => {
       return [item.id,item.getBoundingClientRect()]
     }).reduce((acc, item)=>{return {...acc, [item[0]]:item[1]}},{})
-    console.log(numbers)
-  }
+    console.log(numbers);
+    setTextPos(numbers)
+  },[setTextPos])
+
   return(
     <div
       sx={{height:'100%',width:'100%',backgroundColor:'#131438'}}
@@ -121,15 +155,18 @@ const Splash = () => {
         sx={{height:'100%',width:'100%',backgroundImage:`url(${testPng})`, isolation: 'isolate'}}
       >
         <div sx={{height:'100%',width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-          <div ref={el => getPositions(el)} sx={{height:'auto', width:'85vmin', display:'grid', gridTemplateAreas:"'name vert' 'horz .'"}}>
-            <Name />
-            <TextFlex horizontal/>
+          <div sx={{height:'50px', width:'50px', backgroundColor:'red'}} onClick={()=>triggerAnimation()}></div>
+          <div ref={getPositions} sx={{height:'auto', width:'85vmin', display:'grid', gridTemplateAreas:"'name vert' 'horz .'"}}>
+            <Name horizontal={horizontal}/>
+            <TextFlex horizontal={horizontal}/>
             <TextFlex/>
           </div>
         </div>
         <Cursor />
+        <Developer hx={textPos?.developerH.x}  hy={textPos?.developerH.y} vx={textPos?.developerV.x} vy={textPos?.developerV.y} transition={developerTransition.current} horizontal={horizontal}/>
       </div>
     </div>
+
   )
 
 }
