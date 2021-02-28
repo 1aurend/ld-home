ebc6e1bf7e6c78e3bac70b0d79cfc36b7c951a04
1aurend <32903719+1aurend@users.noreply.github.com>
2021-02-27 23:41:07 -0500

    Update useWheelY

--- src/views/Controller.js
+++ src/views/Controller.js
@@ -1,116 +1,19 @@
-import {
-  useState,
-  useEffect,
-  useRef,
-  createContext
-} from 'react'
+import { createContext } from 'react'
 import useSize from '../hooks/use-debounced-window-size'
 import Layout from './Layout'
 import { yMultiplier, wheelMultiplier } from '../assets/sceneList'
+import useWheelY from '../hooks/use-wheel-y'
 
 export const Y = createContext()
 
 
 export default function Controller() {
   const size = useSize()
-  const prevSize = useRef({width:window.innerWidth,height: window.innerHeight})
-  const ticking = useRef(false)
-  const resize = useRef(false)
-  const eDelta = useRef(0)
-  const prevY = useRef(0)
-  const prevYPercent = useRef(0)
-  const [yPos, setYPos] = useState({px:0,percent:0})
-  console.log(eDelta.current)
-  console.log(yPos)
-
-  useEffect(()=>{
-    const calculateScroll = () => {
-      const yMax = yMultiplier*prevSize.current.height
-      ticking.current = false
-      if (prevY.current - eDelta.current > 0) {
-        prevY.current = 0
-        prevYPercent.current = 0
-        setYPos({px:0,percent:0})
-        return
-      }
-      if ((prevY.current - eDelta.current) < -(yMax)) {
-        prevY.current = -(yMax)
-        prevYPercent.current = 1
-        setYPos({px:yMax,percent:1})
-        return
-      }
-      const nextY = prevY.current - eDelta.current
-      prevY.current = nextY
-      prevYPercent.current = parseFloat((-nextY/(yMax)).toFixed(4))
-      setYPos({px:-nextY,percent:parseFloat((-nextY/(yMax)).toFixed(4))})
-    }
-    const requestTick = () => {
-      if (!ticking.current && !resize.current) {
-        requestAnimationFrame(calculateScroll)
-      }
-      ticking.current = true
-    }
-    const handleWheel = e => {
-      eDelta.current = parseFloat((e.deltaY * wheelMultiplier).toFixed(4))
-      requestTick()
-    }
-    window.addEventListener('wheel', handleWheel)
-    return () => window.removeEventListener('wheel', handleWheel)
-  },[])
-
-  //make this take a target?
-  useEffect(() => {
-    if (size.height !== prevSize.current.height || size.width !== prevSize.current.width) {
-      prevSize.current = size
-      prevY.current = 0
-      prevYPercent.current = 0
-      setYPos({px:0,percent:0})
-    }
-  }, [size])
-
-  const interpolateScrollDown = (target,step=200,buffer=.01) => {
-    const yMax = yMultiplier*prevSize.current.height
-    const nextY = prevY.current - step
-    prevY.current = nextY
-    prevYPercent.current = parseFloat((-nextY/(yMax)).toFixed(4))
-    if (prevYPercent.current > target+buffer) {
-      prevY.current = -target*yMax
-      prevYPercent.current = target
-      setYPos({px:-target*yMax,percent:target})
-      return
-    }
-    setYPos({px:-nextY,percent:parseFloat((-nextY/(yMax)).toFixed(4))})
-    if (prevYPercent.current < target+buffer) {
-      requestAnimationFrame(() => interpolateScrollDown(target))
-    }
-  }
-  const interpolateScrollUp = (target,step=200,buffer=.01) => {
-    const yMax = yMultiplier*prevSize.current.height
-    const nextY = prevY.current + step
-    prevY.current = nextY
-    prevYPercent.current = parseFloat((-nextY/(yMax)).toFixed(4))
-    if (prevYPercent.current < target-buffer) {
-      prevY.current = -target*yMax
-      prevYPercent.current = target
-      setYPos({px:-target*yMax,percent:target})
-      return
-    }
-    setYPos({px:-nextY,percent:parseFloat((-nextY/(yMax)).toFixed(4))})
-    if (prevYPercent.current > target-buffer) {
-      requestAnimationFrame(() => interpolateScrollUp(target))
-    }
-  }
-  const scrollTo = target => {
-    if (prevYPercent.current < target) {
-      requestAnimationFrame(() => interpolateScrollDown(target))
-      return
-    }
-    requestAnimationFrame(() => interpolateScrollUp(target))
-  }
+  const { y, scrollTo } = useWheelY(yMultiplier, wheelMultiplier, size)
 
   return (
-    <Y.Provider value={yPos.percent}>
-      <Layout size={prevSize.current} scrollTo={scrollTo} />
+    <Y.Provider value={y.percent}>
+      <Layout scrollTo={scrollTo} />
     </Y.Provider>
   )
 }
